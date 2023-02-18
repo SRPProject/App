44101d72c5bbfb010437466e6a4e8334ed0749c7
kumaran_virat <86183777+geekykumaran@users.noreply.github.com>
2023-02-18 18:17:52 +0530

    test

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import {BrowserRouter} from "react-router-dom"
+import {useState} from "react"
+
+function App() {
+
+  const [isAuth, changeAuth] = useState<Boolean>(false);
+  const [load,changeLoad] = usSatet
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
