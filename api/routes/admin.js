1222029920f82b0192eb50a53bb029c9f14f01d9
kumaran_virat <86183777+geekykumaran@users.noreply.github.com>
2023-03-18 17:43:04 +0530

    .

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -3,6 +3,11 @@ const router=express.Router()
 const admin=require('../controllers/admin')
 const {verifyAdminToken}=require('../utils/commonfns');
 
+//add student 
+//map student-faculty
+//add regulation and subject 
+//download reports  
+
 router.put('/addDepartment',verifyAdminToken,admin.addDepartments)
 
 module.exports=router;
